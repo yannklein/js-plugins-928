6620da321f5a4a5f826f785fae6215ea5ca008b0
Yann Klein <yann.klein@me.com>
2022-08-03 18:11:55 +0900

    lecture base

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  devServer: {
+    publicPath: '/dist/'
+  },
+  devtool: "sourcemap",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
